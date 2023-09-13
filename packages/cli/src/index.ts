e9453109401252b6ad0465804bb20c768fd342b0
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-09-13 18:12:44 +0100

    Set up CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,27 @@
-export const b = 2;
+#!/usr/bin/env node
+import { program } from "commander";
+
+program
+  .command("init")
+  .argument("[appName]", "app name", "notation-starter")
+  .description("Create Notation App")
+  .action(async (appName) => {
+    console.log(`Creating app in ./${appName}`);
+  });
+
+program
+  .command("compile")
+  .description("Compile Notation App")
+  .action(async () => {
+    console.log("Compiling app");
+  });
+
+program
+  .command("deploy")
+  .argument("[environment]", "environment", "dev")
+  .description("Deploy Notation App")
+  .action(async (stackName) => {
+    console.log(`Deploying stack ${stackName}...`);
+  });
+
+program.parse(process.argv);
