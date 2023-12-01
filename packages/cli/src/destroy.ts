94b5e20421061f849026552095add4a0cdcc0534
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-12-01 16:32:12 +0000

    Ensure orchestration graph up-to-date pre-deploy

--- packages/cli/src/destroy.ts
+++ packages/cli/src/destroy.ts
@@ -1,5 +1,7 @@
 import { destroyApp } from "@notation/core";
+import { compile } from "./compile";
 
 export async function destroy(entryPoint: string) {
+  await compile(entryPoint);
   await destroyApp(entryPoint);
 }
