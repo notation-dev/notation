7a26498256f4e3b1d10f4d6d4955173c256f944d
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-12-08 17:20:36 +0000

    Migrate std library to new resource

--- packages/std.iac/src/fs.ts
+++ packages/std.iac/src/fs.ts
@@ -1,40 +1,46 @@
-import { createResourceFactory } from "@notation/core";
+import { resource } from "@notation/core";
 import path from "node:path";
 import fs from "node:fs/promises";
+import * as z from "zod";
 import * as fflate from "fflate";
 
 export type ZipSchema = {
-  input: { filePath: string };
-  output: { getArrayBuffer: () => Buffer };
-  primaryKey: { filePath: string };
+  Key: { filePath: string };
+  CreateParams: { filePath: string };
+  UpdateParams: { filePath: string };
+  ReadResult: { getArrayBuffer: () => Buffer };
 };
 
-const createZipClass = createResourceFactory<ZipSchema>();
-
-export const Zip = createZipClass({
+const zipResource = resource<ZipSchema>({
   type: "std/fs/Zip",
+});
 
-  getPrimaryKey: (input) => ({
-    filePath: input.filePath,
-  }),
+export const zipSchema = zipResource.defineSchema({
+  filePath: {
+    valueType: z.string(),
+    propertyType: "primaryKey",
+    presence: "required",
+  },
+  getArrayBuffer: {
+    valueType: z.function(z.tuple([]), z.instanceof(Buffer)),
+    propertyType: "computed",
+    presence: "required",
+  },
+});
 
+export const Zip = zipSchema.defineOperations({
   create: async (config) => {
     await zip.package(config.filePath);
-    return zip.read(config.filePath);
+    await zip.read(config.filePath);
   },
 
   read: async (config) => {
-    try {
-      return zip.read(config.filePath);
-    } catch {
-      return null;
-    }
+    return zip.read(config.filePath);
   },
 
   update: async (config) => {
     await zip.delete(config.filePath);
     await zip.package(config.filePath);
-    return zip.read(config.filePath);
   },
 
   delete: async (config) => {
