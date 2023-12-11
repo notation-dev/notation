2c8d4e3a174dd5f60b3c628980fcf3b1114b8e68
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-12-11 23:04:46 +0000

    Get latest orchestration graph in watch mode

--- packages/core/src/orchestrator/graph.ts
+++ packages/core/src/orchestrator/graph.ts
@@ -7,7 +7,9 @@ import { filePaths } from "src/utils/paths";
 
 export async function getResourceGraph(entryPoint: string) {
   const outFilePath = filePaths.dist.infra(entryPoint);
-  await import(path.join(process.cwd(), outFilePath));
+
+  // todo: move into worker thread. this will cause memory leaks
+  await import(path.join(process.cwd(), `${outFilePath}?${Date.now()}`));
 
   const resourceGroups = getResourceGroups();
   const resources = getResources();
