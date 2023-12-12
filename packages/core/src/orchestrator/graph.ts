dd52e3fccbb925adacb698dae5dd8b5cc75dc1a8
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-12-12 00:16:22 +0000

    Fix modules being redeclared in watch mode

--- packages/core/src/orchestrator/graph.ts
+++ packages/core/src/orchestrator/graph.ts
@@ -4,8 +4,10 @@ import {
   getResourceGroups,
 } from "src/orchestrator/state-getters";
 import { filePaths } from "src/utils/paths";
+import { reset } from "..";
 
 export async function getResourceGraph(entryPoint: string) {
+  reset();
   const outFilePath = filePaths.dist.infra(entryPoint);
 
   // todo: move into worker thread. this will cause memory leaks
