283046c2421d160ac8c280d029a029e249ac9bf1
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-12-13 20:50:24 +0000

    Render resources aggregated by resource group

--- packages/dashboard/src/app.tsx
+++ packages/dashboard/src/app.tsx
@@ -5,9 +5,11 @@ import { ResourceList } from "./blocks/resource-list";
 function App() {
   const state = useRemoteState();
   return (
-    <div>
+    <div className="flex flex-col w-screen h-screen overflow-hidden">
       <Header />
-      <ResourceList resources={Object.values(state)} />
+      <div className="flex flex-auto h-full">
+        <ResourceList resources={Object.values(state)} />
+      </div>
     </div>
   );
 }
--- packages/dashboard/src/blocks/resource-list.tsx
+++ packages/dashboard/src/blocks/resource-list.tsx
@@ -2,15 +2,45 @@ import { BaseResource } from "@notation/core";
 
 export function ResourceList(props: { resources: BaseResource[] }) {
   return (
-    <ul>
-      {props.resources.map((resource) => (
-        <li key={resource.id}>
-          <code>
-            <span>{resource.meta.serviceName}</span>.
-            <span>{resource.meta.resourceName}</span>
-          </code>
-        </li>
-      ))}
-    </ul>
+    <div className="w-1/3 border-r">
+      <div className="flex h-full bg-neutral-50 hover:overflow-auto">
+        <ul>
+          {aggregateResources(props.resources).map((resourceGroup) => (
+            <ul
+              key={`${resourceGroup[0].groupType}-${resourceGroup[0].groupId}`}
+              className="mt-3 mb-5"
+            >
+              <li className="px-4 py-2 text-xs text-gray-500 font-semibold">
+                {resourceGroup[0].groupType} #{resourceGroup[0].groupId}
+              </li>
+              <ul>
+                {resourceGroup.map((resource) => (
+                  <li
+                    key={resource.id}
+                    className="px-4 py-3 hover:bg-blue-50 cursor-pointer"
+                  >
+                    <code className="text-xs">{resource.id}</code>
+                  </li>
+                ))}
+              </ul>
+            </ul>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function aggregateResources(resources: BaseResource[]) {
+  const groupedResources: Record<string, BaseResource[]> = {};
+  resources.forEach((resource) => {
+    const key = `${resource.groupId}`;
+    if (!groupedResources[key]) {
+      groupedResources[key] = [];
+    }
+    groupedResources[key].push(resource);
+  });
+  return Object.values(groupedResources).sort((a, b) =>
+    a[0].groupType.localeCompare(b[0].groupType),
   );
 }
