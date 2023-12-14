cd765b5d6e347862fce429100a314e40a3a6ca3c
Gordon Martin <gordonhughmartin@gmail.com>
2023-12-14 18:43:45 +0000

    wip

--- test/compiler.test.app/src/eventbridge/scheduleEvent.fn.ts
+++ test/compiler.test.app/src/eventbridge/scheduleEvent.fn.ts
@@ -1,5 +1,12 @@
+import { LambdaConfig } from "@notation/aws/lambda.fn";
 import { EventBridgeScheduleHandler } from "@notation/aws/shared";
 
 
-export const exampleHandler: EventBridgeScheduleHandler = 
+export const exampleHandler2: EventBridgeScheduleHandler = 
     (event, context) => console.log(JSON.stringify(event))
+
+export const config: LambdaConfig = {
+    service: "aws/lambda",
+    timeout: 5,
+    memory: 64
+    };
