df9f0471a3a8f4a3c2659b855c1549872a228e8f
Gordon Martin <gordonhughmartin@gmail.com>
2023-12-14 16:51:09 +0000

    wip

--- packages/aws/src/event-bridge/schedule.ts
+++ packages/aws/src/event-bridge/schedule.ts
@@ -41,5 +41,16 @@ export const oneTimeSchedule = (dateTime: Date): Schedule => {
 }
 
 export const toAwsScheduleExpression = (schedule: Schedule): string => {
-    throw new Error("wip")
+    if (schedule.type === "rate") {
+        return `rate(${schedule.rate} ${schedule.unit})`
+    } else if (schedule.type === "cron") {
+        return `cron(${schedule.cronExpression})`
+    } else {
+        const dateTime = schedule.dateTime
+        const humanBasedMonth = dateTime.getMonth() + 1
+        const monthString = humanBasedMonth >= 10 ? `${humanBasedMonth}`: `0${humanBasedMonth}`
+        const dayString = dateTime.getDate() >= 10 ? `${dateTime.getDate()}` : `0${dateTime.getDate()}`
+
+        return `at(${dateTime.getFullYear()}-${monthString})-${dayString}-${dateTime.toLocaleTimeString()}`
+    }
 }
