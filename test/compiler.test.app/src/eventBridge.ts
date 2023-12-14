61820c4976f048d81199c380bfbbcb4a42e2b0b5
Gordon Martin <gordonhughmartin@gmail.com>
2023-12-14 19:38:22 +0000

    wip

--- test/compiler.test.app/src/eventBridge.ts
+++ test/compiler.test.app/src/eventBridge.ts
@@ -1,10 +1,10 @@
 import { eventBus, eventBridgeSchedule, rateSchedule } from "@notation/aws/event-bridge"
-import { exampleHandler2 } from "./eventbridge/scheduleEvent.fn"
+import { exampleHandler } from "./eventbridge/scheduleEvent.fn"
 
 eventBus({name: "stuff"})
 
 eventBridgeSchedule(
     rateSchedule(1, "minute"),
     "Event-bridge-schedule-test",
-    exampleHandler2
+    exampleHandler
 )
