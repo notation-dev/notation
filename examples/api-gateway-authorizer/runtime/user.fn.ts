ecd08b9baea68d4d9bd61a10b6d8a6f51e06bf3f
Gordon Martin <gordonhughmartin@gmail.com>
2024-01-22 21:45:02 +0000

    fix

--- examples/api-gateway-authorizer/runtime/user.fn.ts
+++ examples/api-gateway-authorizer/runtime/user.fn.ts
@@ -3,15 +3,15 @@ import { handle, json } from "@notation/aws/lambda.fn";
 import {
   JWTAuthorizedApiGatewayHandler,
   EventWithJWTToken,
+  ApiGatewayHandler,
 } from "@notation/aws/shared";
 import { Context } from "aws-lambda";
 
-export const getUserHandler: JWTAuthorizedApiGatewayHandler =
-  handle.jwtAuthorizedApiRequest(
-    (event: EventWithJWTToken, context: Context) => {
-      return json({ userId: "" });
-    },
-  );
+export const getUserHandler: ApiGatewayHandler = handle.jwtAuthorizedApiRequest(
+  (event: EventWithJWTToken, context: Context) => {
+    return json({ userId: "" });
+  },
+);
 
 export const config: LambdaConfig = {
   service: "aws/lambda",
