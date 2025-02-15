990ef1167e3b7ce72434ef7c11b105b96657b562
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2025-02-15 16:24:10 +0000

    Migrate tailwind

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
   server: {
     proxy: {
       "/state": "http://localhost:6682",
